adb2249aef3cbe476b6a1a8d4328f88c5f35945f
sava <savamedvedev@gmail.com>
2022-06-23 21:31:09 +0800

    2

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @if(\Illuminate\Support\Facades\Auth::user()->is_admin)
+                    <x-nav-link :href="route('admin.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('Admin') }}
+                    </x-nav-link>
+                        @endif
                 </div>
             </div>
 
@@ -82,7 +87,7 @@
 
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                                        this.closest('form').submit()">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
